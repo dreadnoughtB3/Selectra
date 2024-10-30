825da1bf5a002309ed17509e6126a1ef47bf1002
Snoda2023 <tk220137syotaro.noda@gmail.com>
2024-10-30 20:34:19 +0900

    edit metadata

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,8 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Selectra" },
+    { name: "description", content: "サポーターズハッカソン Vol.17用" },
   ];
 };
 
